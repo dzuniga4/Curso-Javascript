a36c4d643bfc85e623de9b4c88d6480e33301624
Luis Cascante <luisalonzocascante@gmail.com>
2021-05-11 21:00:31 -0600

    ejercicios sin solución

--- Clases/Clase 1/ejercicios.js
+++ Clases/Clase 1/ejercicios.js
@@ -1,8 +1,28 @@
 /*
   1. Escriba una función que reciba dos números y devuelva el mayor.
+  2. Escriba una función que dados 3 números devuelva el signo de la multiplicación de los 3. (Positivo o negativo)
+  3. Escriba una función que ordene 3 números de mayor a menor.
+  4. Escriba una función que reciba un idioma ('es', 'en') y devuelva <hola mundo!> en el idioma correspondiente.
+  5. Escriba una función que reciba un número y una palabra, y devuelva un string con la forma correcta, singular o plural.
 */
 
 // 1
 function mayor(numero1, numero2) {
   // completen aquí
+}
+
+function signo(x, y, z) {
+  // complete aquí
+}
+
+function ordenar(x, y, z) {
+  // complete aquí
+}
+
+function holaMundoInternacional(idioma) {
+  // complete aquí
+}
+
+function singularOPlural(numero, palabra) {
+  // complete aquí
 }
