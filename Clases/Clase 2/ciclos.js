b752dc3d534121731558d35f0037ec405a0240e1
Luis Cascante <luisalonzocascante@gmail.com>
2021-05-13 18:52:02 -0600

    ejercicio 1 ciclos

---
+++ Clases/Clase 2/ciclos.js
@@ -0,0 +1,41 @@
+/*
+  ¿Para qué sirve un ciclo?
+  1. While - mientras que la condición se cumpla
+  2. For - tiene una variable, que va aumentando y igual tiene una condición
+*/
+//                0          1        2          3             4
+const arreglo = ["Oscar", "Juan", "Jonathan", "Melissa", "Kenneth"];
+
+// Ciclo While
+let i = 0;
+
+// array.length devuelve la cantidad de elementos
+console.log(arreglo.length);
+//     5 < 5
+while (i < arreglo.length) {
+  console.log("While", arreglo[i]);
+  i++;
+}
+
+let j = 0;
+// Do...while, la diferencia es que corre por lo menos una vez
+do {
+  console.log("Do while", arreglo[j]);
+  i++;
+} while (false);
+
+// For
+//     2        2   < 5            2 + 1
+for (let i = 0; i < arreglo.length; i++) {
+  console.log("For", arreglo[i]);
+}
+
+/*
+  Practica Ciclos
+  1. Escriba una función que ordene 3 números de mayor a menor, NO SE VALE EL .SORT DE ARREGLOS
+*/
+
+// 1
+function ordenar(numero1, numero, numero3) {
+  // complete aquí
+}
