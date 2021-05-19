cabb8f7bcf5f9b8440d68c08d265c8b64f56afbc
Luis Cascante <luisalonzocascante@gmail.com>
2021-05-18 18:15:02 -0600

    clase 2

--- Clases/Clase 2/objetos.js
+++ Clases/Clase 2/objetos.js
@@ -39,25 +39,97 @@ console.log("Tiene propiedad", obj.hasOwnProperty("soyHumano"));
 /* 
   Práctica de objetos
   1. Vamos a crear objetos que tengan el nombre, origen y autor de 1 pinguinos.
-  2. Escriba una funcion que reciba un pinguino, y imprima en pantalla:
-  'Hola, soy un pinguino y me llamo <nombre>
-  3. A algún pinguino, vamos a crearle una nueva propiedad con nombre
-  "puedeVolar" y le vamos a poner false.
-  4. Llamar el método volar del pinguino.
-  5. Vamos a cambiarle el nombre al pinguino a 'Happy Feet'
-  6. Vamos a cambiarle el "puedeVolar" por "true"
-  7. Vamos a crear un método que se llame "volar", si la propiedad "puedeVolar"
+  2. Escriba una método dentro del objeto , y imprima en pantalla:
+  'Hola, soy un pinguino y me llamo <nombre>'
+  3. A  pinguino, vamos a crearle una nueva propiedad con nombre
+  "puedeVolar" y le vamos a poner false, dentro del objeto.
+  4. Vamos a cambiarle el nombre al pinguino a 'Happy Feet'
+  5. Vamos a cambiarle el "puedeVolar" por "true"
+  6. Vamos a crear un método dentro del objeto que se llame "volar", si la propiedad "puedeVolar"
   es true, imprime "Puedo volar!", si es false imprime "No puedo volar :("
-*/
+  7. Llamar el método volar del pinguino.
+  8. Con un ciclo, imprima todas las llaves del objeto pinguino
+  9. Con un ciclo, imprima todos los valores del objeto pinguino
+  */
 
 // 1
-const pinguino1 = {
+console.log(
+  "-----------------------------Ejercicio 1--------------------------"
+);
+const pinguino = {
   nombre: "Misha",
   origen: "Death and the Penguin, Penguin Lost",
   autor: "Andrey Kurkov",
 };
+console.log(pinguino);
 
 // 2
-function presentarse(pinguino) {
-  // inserte código aquí
+console.log(
+  "-----------------------------Ejercicio 2--------------------------"
+);
+pinguino.hola = function () {
+  console.log(`Hola, soy un pinguino y me llamo ${this.nombre}`);
+};
+
+pinguino.hola();
+
+//3
+console.log(
+  "-----------------------------Ejercicio 3--------------------------"
+);
+pinguino.puedeVolar = false;
+console.log(pinguino.puedeVolar);
+
+//4
+console.log(
+  "-----------------------------Ejercicio 4--------------------------"
+);
+pinguino.nombre = "Happy Feet";
+console.log(pinguino.nombre);
+
+//5
+console.log(
+  "-----------------------------Ejercicio 5--------------------------"
+);
+pinguino.puedeVolar = true;
+console.log(pinguino.puedeVolar);
+
+//6
+console.log(
+  "-----------------------------Ejercicio 6--------------------------"
+);
+pinguino.volar = function () {
+  if (this.puedeVolar) {
+    console.log("Puedo volar!");
+  } else {
+    console.log("No puedo volar :(");
+  }
+};
+
+//7
+console.log(
+  "-----------------------------Ejercicio 7--------------------------"
+);
+pinguino.volar();
+
+// 8
+console.log(
+  "-----------------------------Ejercicio 8--------------------------"
+);
+for (llave in pinguino) {
+  console.log(llave);
+}
+
+//9
+console.log(
+  "-----------------------------Ejercicio 9--------------------------"
+);
+for (llave in pinguino) {
+  console.log(pinguino[llave]);
+}
+
+// Extra
+console.log("-----------------------------EXTRA--------------------------");
+for (llave in pinguino) {
+  console.log(llave, pinguino[llave]);
 }
