ceb9d18b046dc4dab5d5a6d0be224b15ae3c34e3
Luis Cascante <luisalonzocascante@gmail.com>
2021-05-25 18:38:58 -0600

    practica array

--- Clases/Clase 5/index.js
+++ Clases/Clase 5/index.js
@@ -5,4 +5,22 @@
  * 2.Escriba una función que le dé vuelta a un arreglo. [1, 2, 3] => [3, 2, 1]
  * 3. Escriba una función que reciba dos arreglos y diga si son iguales  o no. [1, 2], [1, 2] = true
  * [2, 1], [1, 2] = false
+ * 4. Escriba una función que reciba un arreglo de pares llave/valor y devuelva un objeto.
+ *  [
+      ['nombre', 'Luis'], 
+      ['apellido', 'Cascante']
+    ]
+ * 
+ * {
+      nombre: 'Luis',
+      apellido: 'Cascante'
+    }
+  *5. Escriba una función que reciba un arreglo y devuelva un arreglo sin elementos duplicados, PERO no pueden usar el new Set() de JS. [1, 2, 3, 1, 2] => [1, 2, 3]
+  *6. Escriba una función que reciba dos arreglos y devuelva la intersección.
+  7. Escriba una función que reciba un variable y diga si es un arreglo.
+  8. Escriba una función que reciba un arreglo y lo clone, es decir, devuelva un arreglo igual.
+  9. Escriba una función que reciba un arreglo de strings y devuelva todos los strings concatenados.
+  10. Escriba una función que reciba un número e inserte un '-' menos entre los números pares y un '*' asterico entre dos números impares. '134245780' => '1*34-2-45*78-0'.
+  11. 
+
  */
