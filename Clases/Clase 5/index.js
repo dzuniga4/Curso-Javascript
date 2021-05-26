8d7d44dd997abcc7b6c4d7e02a3e1e365c7f177e
Luis Cascante <luisalonzocascante@gmail.com>
2021-05-25 18:36:52 -0600

    practica array

---
+++ Clases/Clase 5/index.js
@@ -0,0 +1,8 @@
+/**
+ * Práctica de Arreglos
+ *
+ * 1. Escriba una función que reciba un contador, y un valor y cree un arreglo con la cantidad de valores que dice el contador. llenar(3, 'b') => ['b', 'b', 'b']
+ * 2.Escriba una función que le dé vuelta a un arreglo. [1, 2, 3] => [3, 2, 1]
+ * 3. Escriba una función que reciba dos arreglos y diga si son iguales  o no. [1, 2], [1, 2] = true
+ * [2, 1], [1, 2] = false
+ */
