abe2e31d0aa82aa5b0b43fcf5038ba3d616f37c3
Luis Cascante <luisalonzocascante@gmail.com>
2021-05-25 18:49:22 -0600

    practica array

--- Clases/Clase 5/index.js
+++ Clases/Clase 5/index.js
@@ -16,11 +16,14 @@
       apellido: 'Cascante'
     }
   *5. Escriba una función que reciba un arreglo y devuelva un arreglo sin elementos duplicados, PERO no pueden usar el new Set() de JS. [1, 2, 3, 1, 2] => [1, 2, 3]
-  *6. Escriba una función que reciba dos arreglos y devuelva la intersección.
+  *6. Escriba una función que reciba dos arreglos y devuelva la intersección. devolver los elementos que están en los dos arreglos.
   7. Escriba una función que reciba un variable y diga si es un arreglo.
   8. Escriba una función que reciba un arreglo y lo clone, es decir, devuelva un arreglo igual.
   9. Escriba una función que reciba un arreglo de strings y devuelva todos los strings concatenados.
   10. Escriba una función que reciba un número e inserte un '-' menos entre los números pares y un '*' asterico entre dos números impares. '134245780' => '1*34-2-45*78-0'.
-  11. 
-
+  11. Escriba una función que "revuelva" un arreglo. O sea, que cambie los elementos de posición al azar. Pueden usar Math.random()
+  12. Escriba una función que encuentre los valores repetidos de un arreglo.
+  13. Escriba una función que encuentre la diferencia de dos arreglos, es decir, devolver los valores que solo están en uno y no en el otro.
+  14. EXTRA EXTRA: Escriba una función que 'aplane' un arreglo. [[1, 2], 3, 4, 5 ,[6, 7, 8]] =
+  [1, 2, 3, 4, 5, 6, 7, 8]
  */
