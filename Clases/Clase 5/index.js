4b926fe23a13b30a4e9231ced9df4bc9d0c7228c
dzuniga4 <82187454+dzuniga4@users.noreply.github.com>
2021-05-27 21:52:53 -0600

    Update index.js

--- Clases/Clase 5/index.js
+++ Clases/Clase 5/index.js
@@ -1,8 +1,22 @@
+
 /**
  * Práctica de Arreglos
  *
- * 1. Escriba una función que reciba un contador, y un valor y cree un arreglo con la cantidad de valores que dice el contador. llenar(3, 'b') => ['b', 'b', 'b']
- * 2.Escriba una función que le dé vuelta a un arreglo. [1, 2, 3] => [3, 2, 1]
+ * 1. Escriba una función que reciba un contador, y un valor y cree un arreglo con la cantidad de valores que dice el contador. 
+ * llenar(3, 'b') => ['b', 'b', 'b']
+*/
+function Practica1(contador,c){
+   const arreglo = []
+  for(i=0;i<=contador;i++){
+    arreglo.push(c);
+  }
+
+}
+const resultado1 = Practica1(3,'b');
+    console.log(resultado);
+
+
+/* 2.Escriba una función que le dé vuelta a un arreglo. [1, 2, 3] => [3, 2, 1]
  * 3. Escriba una función que reciba dos arreglos y diga si son iguales  o no. [1, 2], [1, 2] = true
  * [2, 1], [1, 2] = false
  * 4. Escriba una función que reciba un arreglo de pares llave/valor y devuelva un objeto.
